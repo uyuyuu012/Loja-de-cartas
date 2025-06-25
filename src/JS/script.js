8556a5c256560eac4300ba4e8cf062fbd606c5c8
Guilherme <guitama.games@gmail.com>
2025-06-24 22:44:28 -0300

    Terminando o Javascript e o hamburguer

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,17 +1,7 @@
-//DECLARAÇÃO DE VARIAVEIS CHAMANDO AS CLASSES COM DOM  
-const hamburguer = document.querySelector('.hamburguer');
-const headerMenu = document.querySelector('.menu-header');
+const hamburguer = document.getElementById('hamburguer');
+const nav = document.querySelector('nav.item-menu');
 
-//CRIANDO A FUNÇÃO QUE VAI MOSTRAR OU OCULTAR O MENU
-function toggleMenu(){
-    hamburguer.classList.toggle('active'); //CRIA A CLASSE ACTIVE SE NÃO EXISTIR
-    headerMenu.classList.toggle('active');  //CRIA A CLASSE ACTIVE SE NÃO EXISTIR
-}
-
-//EVENTO AO CLICAR NA BARRINHA  E MOSTRA OS ITEMS DO MENU
-hamburguer.addEventListener('click', toggleMenu);
-headerMenu.addEventListener('click', (event) => {
-    if (event.target.classList.contains('item-menu')) {
-        toggleMenu();
-    }
+hamburguer.addEventListener('click', () => {
+    hamburguer.classList.toggle('active');
+    nav.classList.toggle('active');
 });
